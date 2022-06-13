a8b4ecaa6c51d3ec110646109840a1f79bbe8461
Tassos Makrostergios <86314553+TASMAK@users.noreply.github.com>
2022-06-13 19:42:20 +0300

    fix

--- Kernel.py
+++ Kernel.py
@@ -1,20 +1,13 @@
-'''
-PyTerminal 
-'''
+
 import os
 import commands
 
-
-
 cmd = commands
-
-
 
 def history():
     with open('history.log', 'w') as f:    
         f.write(str(cmd.LCommand))
-        
-        
+                
 def core(MODE="0"):
     if MODE == "1":
         cmd.CommandList()
