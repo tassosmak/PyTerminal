e01cd104164e3a8f07d104305b4c7475b82f0ddd
Tassos Makrostergios <86314553+TASMAK@users.noreply.github.com>
2022-08-10 13:48:19 +0300

    Removed Comments

--- launcher.py
+++ launcher.py
@@ -7,7 +7,6 @@ def ask():
     global ask_core
     print("there are 2 Modes on this terminal:\n1) The Basic Mode,     2) The Advanced Mode")
     ask_core = input("select Mode")
-#jump_Done = False
 ask()
 cmd.MD = ask_core
 print("Go Ahead")
