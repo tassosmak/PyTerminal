89c8c4c6716c8f63d556b23b0b6d963130c6e4cd
Tassos Makrostergios <86314553+TASMAK@users.noreply.github.com>
2022-08-10 15:13:11 +0300

    User ask added STILL WORK IN PROGRESS

---
+++ UserHandler.py
@@ -0,0 +1,45 @@
+import commands
+cmd = commands
+
+
+def ask():
+    global username_ask
+    username_ask = input("Enter Usename")
+    #print("there are 2 Modes on this terminal:\n1) The Basic Mode,     2) The Advanced Mode")
+    #ask_core = input("select Mode")
+    UserSearch = open("UserList.log", "r")
+    if(username_ask in UserSearch.read()):
+        print("ok")
+    else:
+        NewUser = input("This Username Doesn't exist do you want to create a user with this name")
+        if NewUser == "Y" or NewUser == "y":
+            '''
+            init phase
+            '''
+            UserSearch = open("UserList.log", "a")
+            ask_UserMD = open("UserListMD.log", "a")
+            
+            '''
+            ask phase
+            '''
+            ask_UserMD = input("there are 2 Modes on this terminal:\n1) The Basic Mode,     2) The Advanced Mode\nChoose One!")
+            
+            
+
+            '''
+            append phase
+            '''
+            UserSearch.write(str(f"{username_ask}{ask_UserMD}\n"))
+            
+            ask_UserMD.write(str(f"{ask_UserMD}\n"))
+ask()
+
+
+
+
+
+
+#cmd.MD = ask_core
+print("Go Ahead")
+
+
--- launcher.py
+++ launcher.py
@@ -1,22 +1,17 @@
 import Kernel
 import commands
+import UserHandler
+UserH = UserHandler
 kernel = Kernel
 cmd = commands
 
-def ask():
-    global ask_core
-    print("there are 2 Modes on this terminal:\n1) The Basic Mode,     2) The Advanced Mode")
-    ask_core = input("select Mode")
 
-ask()
-cmd.MD = ask_core
-print("Go Ahead")
 while True:
-    kernel.core(MODE=ask_core)
+    kernel.core(MODE=UserH.ask_core)
     #cmd.CommandAsk()
     if cmd.jump:
-        ask()
-        cmd.MD = ask_core
-        kernel.core(MODE=ask_core)
+        UserH.ask()
+        cmd.MD = UserH.ask_core
+        kernel.core(MODE=UserH.ask_core)
         cmd.jump = False
     #print(cmd.jump)
