52c79a2d99d354582960686f0bca81aaaef0b489
Tassos Makrostergios <86314553+TASMAK@users.noreply.github.com>
2022-11-11 22:40:19 +0200

    Removed Useless ifs

--- launcher.py
+++ launcher.py
@@ -19,18 +19,17 @@ def run():
             UserH.Change_Listed_MODE(ask_new_md)
         if cmd.jump:
             try:
-                if not restrict_jump:
-                    UserH.pl_finder()
-                    ask()
-                    settings.MODE = ask_core
-                    try:
-                        kernel.core(MODE=ask_core, pl=settings.pl, username=UserH.username_ask)
-                    except IndexError:
-                        ask_corect_md = input("the selected mode doesn't exist These is the available options\n1) The Basic Mode\n2)The Advanced Mode\nType below:\n ")
-                        settings.MODE = ask_corect_md
-                        kernel.core(MODE=ask_corect_md, pl=settings.pl, username=UserH.username_ask)
-                    cmd.CommandSay(answer="this is only for the current sension\nthe next time it will be restored\nto the previous state", color="WARNING")
-                    cmd.jump = False
+                UserH.pl_finder()
+                ask()
+                settings.MODE = ask_core
+                try:
+                    kernel.core(MODE=ask_core, pl=settings.pl, username=UserH.username_ask)
+                except IndexError:
+                    ask_corect_md = input("the selected mode doesn't exist These is the available options\n1) The Basic Mode\n2)The Advanced Mode\nType below:\n ")
+                    settings.MODE = ask_corect_md
+                    kernel.core(MODE=ask_corect_md, pl=settings.pl, username=UserH.username_ask)
+                cmd.CommandSay(answer="this is only for the current sension\nthe next time it will be restored\nto the previous state", color="WARNING")
+                cmd.jump = False
             except NameError:
                     UserH.pl_finder()
                     ask()
@@ -39,12 +38,11 @@ def run():
                     kernel.core(MODE=ask_core, pl=settings.pl, username=UserH.username_ask)
                     cmd.jump = False
         if cmd.jump_user:
-            if not restrict_jump:
-                UserH.pl_finder()
-                UserH.ask()
-                cmd.MD = UserH.UserMD
-                cmd.jump_user = False
-                kernel.core(MODE=settings.MODE, pl=settings.pl, username=UserH.username_ask)
+            UserH.pl_finder()
+            UserH.ask()
+            cmd.MD = UserH.UserMD
+            cmd.jump_user = False
+            kernel.core(MODE=settings.MODE, pl=settings.pl, username=UserH.username_ask)
 
 
 UserH.pl_finder()
