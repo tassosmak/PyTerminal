cd7b1f712960449438147bcd51fac2926befe246
tassosmak <tasos23072007@outlook.com.gr>
2023-01-01 13:15:20 +0200

    Fixes

--- CryptographyKit/DecryptPassword.py
+++ CryptographyKit/DecryptPassword.py
@@ -1,13 +1,18 @@
 import json
 import base64
+from RendererKit import Renderer as RD
+from os import _exit
 
 def decrypt_password(key, password):
     # Load the encrypted password and key from the file
-    f = open("MakroPropiatery.json")
-    data = json.load(f)
-    Password = data['user_credentials']['Password']
-    key_json = data['user_credentials']['Key']
-
+    try:
+        f = open("MakroPropiatery.json")
+        data = json.load(f)
+        Password = data['user_credentials']['Password']
+        key_json = data['user_credentials']['Key']
+    except FileNotFoundError:
+        RD.CommandSay(answer='You Dont Have the Privilages to Enter This Mode')
+        _exit(1)
     while key != key_json:
         key = input("Enter key: ")
     
