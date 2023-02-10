6fc20cc47414b738aa8c26d4ecd0d2e5cb3329ac
tassosmak <tasos23072007@outlook.com.gr>
2023-02-10 18:38:47 +0200

    Bug Fixes

--- Kernel/AudioKit/Audio.py
+++ Kernel/AudioKit/Audio.py
@@ -6,6 +6,8 @@ from Kernel import flags
 def play(file):
     try:
         if flags.EnableAudio:
-            subprocess.run(f'mpg123 {file}', shell=True, capture_output=True , check=True, encoding="utf-8")
+            subprocess.run(f'mpg12 {file}', shell=True, capture_output=True , check=True, encoding="utf-8")
     except:
-        RD.CommandSay("AudioKit failed", 'FAIl')
+        RD.CommandSay("AudioKit failed", 'FAIL')
+        from Kernel.utils import edit_json
+        edit_json(loc1='UI', loc2='Enable-Audio', content='0')
