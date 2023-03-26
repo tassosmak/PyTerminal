84dd1eac6a3a4c8f8d880f9ba2d14dba62059189
tassosmak <tasos23072007@outlook.com.gr>
2023-03-26 18:27:17 +0300

    Bug Fixes

--- Kernel/builtin/Password_Gen.py
+++ Kernel/builtin/Password_Gen.py
@@ -1,9 +1,10 @@
 from random import shuffle, choice
 import string
 
-from src import utils
-utils.add_depend(str(utils.sys.argv[1]))
+from src.utils import add_depend, sys
+add_depend(str(sys.argv[1]))
 from Kernel.RendererKit import Renderer as RD
+from Kernel import flags
 
 characters = list(string.ascii_letters + string.digits + "!@#$%^&*()")
 RD.CommandQuest(type='3', msg="How long do you want your password to be")
@@ -18,6 +19,6 @@ password = ''.join(password)
 RD.CommandPush(message=(f'Your Password Is: {password}'))
 RD.CommandQuest(type='1', msg="Would You like to export the password to a text file")
 if RD.Quest_result == "Yes":
-    f = open("password.txt", "w")
-    f.write(password_str)
+    with open(f"{flags.base_folder}/../password.txt", "w") as f:
+        f.write(password_str)
     RD.CommandPush('The File Is Saved')
