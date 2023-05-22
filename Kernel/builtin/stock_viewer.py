6a31d31c65753fa6a299d2695512007f30d08292
tassosmak <tasos23072007@outlook.com.gr>
2023-05-22 14:42:24 +0300

    Bug Fixes

--- Kernel/builtin/stock_viewer.py
+++ Kernel/builtin/stock_viewer.py
@@ -3,15 +3,11 @@ add_depend(str(sys.argv[1]))
 from Kernel.RendererKit import Renderer as RD
 
 import yfinance as yf
-while True:
-    Stock = RD.CommandQuest(type='3', msg='Enter Your Stock', header='Stock Viewer')
-
+Stock = RD.CommandQuest(type='3', msg='Enter Your Stock', header='Stock Viewer')
+    
+if not Stock.lower() == 'exit':
     ticker = yf.Ticker(Stock).info
     market_price = ticker['regularMarketOpen']
     previous_close_price = ticker['regularMarketPreviousClose']
 
-    # RD.CommandSay(f'Selected Stock: {Stock}', 'GREEN')
-
-    # RD.CommandSay((f'Market Price:, {market_price}'))
-    # RD.CommandSay((f'Previous Close Price: {previous_close_price}'))
     RD.CommandQuest(type='2', msg=f'Market Price: {market_price}\nPrevious Close Price: {previous_close_price}', header=f'Stock Viewer | Selected Stock: {Stock}')
