cef90164543864054fd19bf8b73f0287e7c9aede
tassosmak <tasos23072007@outlook.com.gr>
2025-06-24 16:47:04 +0300

    Refactoring of CommandAsk

--- Kernel/Input_Output.py
+++ Kernel/Input_Output.py
@@ -1,34 +1,35 @@
 from Kernel.RendererKit import Renderer as RD
 from Kernel import flags
 
-
 def CommandAsk(Module=str, command=str):
-    #MODE 2
+    # Mode 2
     if flags.MODE == "2":
+        prompt = f"{flags.MD2} | {RD.bcolors.OKBLUE}{flags.USERNAME.capitalize()}{RD.bcolors.WHITE} % "
         if flags.Runtype == 'local':
-            Module(Command=input(f"{flags.MD2} | {RD.bcolors.OKBLUE}{flags.USERNAME.capitalize()}{RD.bcolors.WHITE} % ").lower())
-        elif flags.Runtype == 'gui':
-            Module(Command=command)
-    #MODE 9
-    elif flags.MODE == "9" and flags.BuildReseted == False:
-        #GUI
+            return Module(Command=input(prompt).lower())
+        return Module(Command=command)
+
+    # Mode 9
+    if flags.MODE == "9" and not flags.BuildReseted:
+        prompt = f"{flags.MD9} {flags.sys_detect.system} | {flags.sys_detect.machine} % "
         if flags.Run_Straight_Builtin:
-            Module(Command=input(f"{RD.bcolors.WARNING}Run-Straight-Builtin Enabled{RD.bcolors.WHITE} | {flags.MD9} {flags.sys_detect.system} | {flags.sys_detect.machine} % ").lower())
-        elif flags.Fully_GUI:
+            msg = f"{RD.bcolors.WARNING}Run-Straight-Builtin Enabled{RD.bcolors.WHITE} | {prompt}"
+            return Module(Command=input(msg).lower())
+        if flags.Fully_GUI:
             RD.CommandShow(msg=f"{flags.MD9} {flags.sys_detect.system} | {flags.sys_detect.machine} | Expreimental GUI").Input()
-            try: Module(Command=RD.Quest_result.lower())
-            except AttributeError: RD.CommandShow('\n').Show()
-        #Non GUI
-        else:
-            if flags.Runtype == 'local':
-                Module(Command=input(f"{flags.MD9} {flags.sys_detect.system} | {flags.sys_detect.machine} % ").lower())
-            elif flags.Runtype == 'gui':
-                Module(Command=command)#(f"{flags.MD9} {flags.sys_detect.system} | {flags.sys_detect.machine} % ").lower())
-            
-    #Safe Mode
-    elif flags.MODE == "3":
-        Module(Command=input(f'{RD.bcolors.WARNING}{flags.MD3}{RD.bcolors.WHITE}').lower(), safe_md=True)
-        
-    #MODE 1
-    else:
-        Module(Command=input(f"{flags.Default_text} | {RD.bcolors.OKCYAN}{flags.USERNAME.capitalize()}{RD.bcolors.WHITE} $ ").lower())
+            try:
+                return Module(Command=RD.Quest_result.lower())
+            except AttributeError:
+                return RD.CommandShow('\n').Show()
+        if flags.Runtype == 'local':
+            return Module(Command=input(prompt).lower())
+        return Module(Command=command)
+
+    # Safe Mode 3
+    if flags.MODE == "3":
+        prompt = f'{RD.bcolors.WARNING}{flags.MD3}{RD.bcolors.WHITE}'
+        return Module(Command=input(prompt).lower(), safe_md=True)
+
+    # Default Mode 1
+    prompt = f"{flags.Default_text} | {RD.bcolors.OKCYAN}{flags.USERNAME.capitalize()}{RD.bcolors.WHITE} $ "
+    return Module(Command=input(prompt).lower())
