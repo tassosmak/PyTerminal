79a1e9a7160463913c31f478b4b35e8839ce5747
tassosmak <tasos23072007@outlook.com.gr>
2025-06-25 12:31:23 +0300

    typo

--- Kernel/flags.py
+++ Kernel/flags.py
@@ -146,7 +146,7 @@ ACML = [
 "gui",
 ]
 
-MD2 = "!Advanched Mode! PyTerminal Beta"
+MD2 = "!Advanced Mode! PyTerminal Beta"
 MD3 = "PyTerminal | Native-Mode $ "
 MD9 = "PyTerminal"
 Default_text = 'Makro PyTerminal'
